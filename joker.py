d83832a6351d406f34144f48bd4a140e63b3967b
Konstantine Kahadze <konstantinekahadze@gmail.com>
2022-05-07 22:34:34 -0400

    Wrote most method for interactive game

---
+++ joker.py
@@ -0,0 +1,239 @@
+import numpy as np
+import random
+import datetime
+import numpy as np
+from more_itertools import quantify
+
+def get_obs(self):
+    return (self._ord, self._tot - 1, self._jok, self._btl, int(self._tmc + 8))
+
+def set_players_cards(self):
+    self._table = [list(), list(), list(), list()]
+    self._deck = Deck()
+    self._deck.shuffle()
+    for hand in self._table:
+        self._deck.deal(hand, times=9)
+    self._jok = quantify(map(lambda x: x.value == 13, self._table[0]))
+
+def set_calls(self):
+    self.calls = np.zeros((4))
+    for i in range(4):
+        cur_player = (self.first_to_play + i) % 4
+        want = quantify(map(lambda x: x.value > 11, self._table[cur_player])) 
+        if want > 0:
+            want-=1
+        if i < 3 or want == 9 - self.calls.sum():
+            self.calls[cur_player] = want
+        else:
+            if want > 0:
+                self.calls[cur_player] = want
+            else:
+                self.calls[cur_player] = 0
+    self._tmc = -1 * self.calls[0]
+
+def playable(self, player):
+    all = self._table[player]
+    firsts = list(filter(lambda x: x.suit == self.first_suit, all))
+    if not firsts:
+        return all
+    else:
+        firsts.extend(list(filter(lambda x: x.value == 13, all)))
+        return firsts
+
+def choose_strg_beat(self, playable): 
+    highest = get_highest(playable, self.first_suit) 
+    return highest if highest else get_highest(playable) 
+
+def choose_weak_beat(self, playable): 
+    beats = self._get_beats(playable) 
+    if not beats:
+        return self._choose_weak_loss(playable)
+    lowest = Card(13, 0)
+    for card in beats:
+        if card.value < lowest.value:
+            lowest = card
+    return lowest
+
+def choose_strg_loss(self, playable): 
+    loses = self._get_loses(playable)
+    if not loses:
+        return self._choose_strg_beat(playable)
+    else:
+        highest = get_highest(playable, suit=self.first_suit)
+        return highest if highest else get_highest(playable)
+
+def choose_weak_loss(self, playable):
+    acc = Card(13, 0); [acc := Card(card.value, card.suit) for card in playable if card.value < acc.value]
+    return acc
+
+def get_winning_card(self):
+    if not self.played.any():
+        return None
+    elif quantify(map(lambda x: x and x.value == 13, self.played)): # No shadow jokers
+        return Card(13,0)
+    else:
+        firsts = list(filter(lambda x: x and x.suit == self.first_suit, self.played))
+        firsts[np.argmax([card.value for card in firsts])]
+
+def get_beats(self, playable):
+    cur_winner = self._get_winning_card() # returning None
+    if self.first_suit == None or not cur_winner:
+        return playable
+    else:
+        if cur_winner.value == 13:
+            return list(filter(lambda x: x.value == 13, playable))
+        else:
+            return list(filter(lambda x: x and x.value > cur_winner.value, playable))
+
+def get_loses(self, playable):
+    if self.first_suit == None:
+        return playable
+    loses = []
+    for card in playable:
+        if card.suit == self.first_suit and card.value < max([y.value for y in list(filter(lambda x : x and x.suit == self.first_suit, playable))]):
+            loses.append(card)
+    return loses
+
+def card_to_weight(self, card):
+    weight = 0
+    if card.value == 13:
+        return 200
+    if self.first_suit == card.suit:
+        weight += 100
+    return weight + card.value  
+
+def winner(self, first):
+    jok = 5
+    joks = 0
+    for i in range(4):
+        cur = (first + i) % 4
+        
+        if self.played[cur].value == 13:
+            jok = cur
+            joks += 1
+    if joks:
+        return jok
+    else:
+        weights = []
+        for i in range(4):
+            weights.append(self._card_to_weight(self.played[i]))
+        return np.argmax(weights)
+
+def remove_from_table(self, player, suit, value):
+    for card in self._table[player]:
+        if (card.suit, card.value) == (suit, value):
+            self._table[player].remove(card)
+            break
+
+def play_rand(self, player):
+    poss = self._playable(player)
+    choice = poss[random.randint(0, len(poss) - 1)]
+    self._remove_from_table(player,choice.value, choice.suit)
+    self.played[player] = choice
+    return choice
+
+def pre_plays(self):
+    self.played = np.ndarray((4), dtype=Card)
+    for i in range(self._ord):
+        cur = (self.first_to_play + i) % 4
+        temp = self._play_rand(cur)
+        self.played[cur] = temp
+        if i == 0 and self.played[cur].value != 13:
+            self.first_suit = self.played[cur].suit
+    self._btl = 1 if self._get_beats(self._playable(0)) else 0
+
+def post_plays(self):
+    for i in range(1, 4 - self._ord):
+        cur = i % 4
+        self.played[cur] = self._play_rand(cur)
+
+    self.hand_winner = self._winner()
+    self.first_suit = None
+    self.played = None
+    self._set_players_cards()
+    self._set_calls() 
+
+def get_highest(cards ,suit=None):
+    '''Returns a joker if there is one in cards.
+        Otherwise returns the highest valued card of the specified suit'''
+    if not suit:
+        return cards[np.argmax([card.value for card in cards])]
+    else:
+        acc = None;[acc := Card(x,y) for (x, y) in map(lambda card: (card.value, card.suit), cards) if (not acc) or (suit == y and x > acc.value) or x == 13]
+        return acc if acc else None
+    
+class Card(object):
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        values = {
+            4: lambda: "Six",
+            5: lambda: "Seven",
+            6: lambda: "Eight",
+            7: lambda: "Nine",
+            8: lambda: "Ten",
+            9: lambda: "Jack",
+            10: lambda: "Queen",
+            11: lambda: "King",
+            12: lambda: "Ace",
+            13: lambda: "Joker",
+        }
+        suits = {
+            0: lambda : "Diamonds",
+            1: lambda : "Clubs",
+            2: lambda : "Hearts",
+            3: lambda : "Spades",
+        }
+        
+        value_name = values[self.value]()
+        suit_name = suits[self.suit]()
+
+        if value_name == "Joker":
+            return "Joker"
+        else:
+            return value_name + " of " + suit_name
+
+class Deck(list):
+    def __init__(self):
+        super().__init__()
+        suits = list(range(4))
+        values = list(range(5, 13))
+        # Ranks 7 through A are added
+        [[self.append(Card(i, j)) for j in suits] for i in values]
+        # Ranks Six and Ace are added
+        self.extend([Card(13, 0), Card(13, 0), Card(4, 0), Card(4, 2)]) 
+
+    def __repr__(self):
+        out = ""
+        for card in self:
+            out += (str(card) + "\n")
+        return out
+
+    def shuffle(self):
+        random.seed(datetime.datetime.now().timestamp())
+        random.shuffle(self)
+
+    def get(self, index):
+        return self[index]
+
+    def deal(self, location, times=1):
+        for _ in range(times):
+            location.append(self.burn())
+
+    def burn(self):
+        return self.pop(0)
+
+class Player(object):
+    def __init__(self, id_in):
+        self.id = id_in
+        self.score = 0
+        self.cards = []
+        self.called = 0
+        self.taken = 0
+
+    def __repr__(self):
+        id = self.id
+        return id
+
---
+++ nines_gym.py
@@ -0,0 +1,115 @@
+import gym
+from gym import spaces
+import numpy as np
+from joker import *
+import random
+
+class NineEnv(gym.Env):
+    metadata = {"render_modes": ["human"], "render_fps": 2}
+
+    def __init__(self):
+        self.observation_space = spaces.Tuple((
+            spaces.Discrete(4),
+            spaces.Discrete(9),
+            spaces.Discrete(3),
+            spaces.Discrete(4),
+            spaces.Discrete(18),
+        ))
+        self.action_space = spaces.Discrete(4)
+
+    def reset(self, seed=None):
+        # We need the following line to seed self.np_random
+        # super().reset()
+
+        self.played = np.ndarray((4), dtype=Card)
+        self.first_to_play = random.randint(0, 3)
+
+        self._ord = (4 - self.first_to_play) % 4
+        self._tot = 9
+
+        self._set_players_cards() # this has to set self._jok
+        self._set_calls() # this has to set self._tmc
+
+        self.first_suit = None
+
+        if self.first_to_play != 0: 
+            self._pre_plays()
+        else:
+            self._btl = 1
+        observation = self._get_obs()
+
+        return observation
+
+    def step(self, action): 
+        # Map the action (element of {0,1,2,3}) to the card we play
+        self._act(action)
+        self._post_plays()
+        self._set_players_cards() 
+        self._set_calls()
+        self._tot -= 1
+
+        if not self.hand_winner:
+            self._tmc += 1
+        
+        self.first_to_play = self.hand_winner
+        self._ord = (4 - self.first_to_play) % 4
+
+        self.played = None
+        self._pre_plays()
+        done = self._tot == 0
+        reward = 1 if self._tmc == 0 and done else 0   # No negative rewards
+        observation = self._get_obs()
+        
+        # No info
+        return observation, reward, done, None
+
+    def _act(self, action):
+        playable = self._playable(0)
+        if action == 'STRG-BEAT':
+            card = self._choose_strg_beat(playable)
+        elif action == 'WEAK-BEAT':
+            card = self._choose_weak_beat(playable)
+        elif action == 'STRG-LOSS':
+            card = self._choose_strg_loss(playable)
+        elif action == 'WEAK-LOSS':
+            card = self._choose_weak_loss(playable)
+        else:
+            return None
+
+        if self._ord == 0:
+            self.first_suit = card.value
+            self.played = np.ndarray((4), dtype=Card)
+            self.played[0] = card
+        elif self._ord == 3: 
+            self.first_suit = None
+            self.played[0] = card
+            self.hand_winner = self._winner(self.first_to_play)
+        else:
+            self.played[0] = card
+
+        if card and card.value == 13:
+            for card in self._table[0]:
+                if card.value == 13:
+                    self._table[0].remove(card)
+                    break
+        else:
+            self._remove_from_table(0, card.suit, card.value)
+        return card
+    
+    # Joker game related functions
+    _get_obs = get_obs
+    _set_players_cards = set_players_cards
+    _set_calls = set_calls
+    _playable  = playable
+    _choose_strg_beat = choose_strg_beat
+    _choose_strg_loss = choose_strg_loss
+    _choose_weak_beat = choose_weak_beat
+    _choose_weak_loss = choose_weak_loss
+    _play_rand = play_rand
+    _winner = winner
+    _pre_plays, _post_plays = pre_plays, post_plays
+    _remove_from_table = remove_from_table
+    _get_beats = get_beats
+    _get_winning_card = get_winning_card
+    _card_to_weight = card_to_weight
+    _get_loses = get_loses
